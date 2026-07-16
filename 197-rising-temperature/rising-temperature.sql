4351908369ea91dea145d21a6ca31329ce36c7c4
rithwikab <66661057+rithwikab@users.noreply.github.com>
2026-07-16 18:38:57 +0530

    Time: 387 ms (93.04%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select w1.id from Weather w1 join Weather w2 where w2.recordDate = date_sub(w1.recordDate, Interval 1 day) and w1.temperature > w2.temperature;
